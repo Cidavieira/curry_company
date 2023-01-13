a2aeae91d0d46b648ff32716f5339fc646b9d43c
Cida Vieira <95623644+Cidavieira@users.noreply.github.com>
2023-01-12 22:00:59 -0300

    Update 2_visao_entregadores.py

--- pages/2_visao_entregadores.py
+++ pages/2_visao_entregadores.py
@@ -97,8 +97,8 @@ df1= clean_code(df)
 st.header('Marketplace - Visão Entregadores')
 
 
-image=Image.open('image.png')
-st.sidebar.image(image,width=120)
+#image=Image.open('image.png')
+#st.sidebar.image(image,width=120)
 
 st.sidebar.markdown('# Cury Company')
 st.sidebar.markdown('## Fastest Delivery in Town')
