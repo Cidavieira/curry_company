e18fddf9ea405f1ef8e108d5a848e8b69bf87e02
Cida Vieira <95623644+Cidavieira@users.noreply.github.com>
2023-05-19 14:24:50 -0300

    Update 3_visao_restaurantes.py

--- pages/3_visao_restaurantes.py
+++ pages/3_visao_restaurantes.py
@@ -9,6 +9,7 @@ import numpy as np
 from PIL import Image 
 from haversine import haversine
 from streamlit_folium import folium_static
+from datetime import datetime
 
 st.set_page_config(page_title='Visão Restaurantes',layout='wide')
 #===========================================
@@ -157,9 +158,9 @@ st.sidebar.markdown('## Selecione uma data limite')
 
 date_slider=st.sidebar.slider(
                   'Até qual valor?',
-                   value=pd.datetime(2022,4,13),
-                   min_value=pd.datetime(2022,2,11),
-                   max_value=pd.datetime(2022,4,6),
+                   value=datetime(2022,4,13),
+                   min_value=datetime(2022,2,11),
+                   max_value=datetime(2022,4,6),
                     format='DD-MM-YYYY')
 
 st.sidebar.markdown("""---""")
